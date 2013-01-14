b2b04171b806e6cfc7036c17e0b3428357718966
Matt Jeanes <mattjeanes23@gmail.com>
2013-01-14 17:49:52 +0000

    Linux Fixes

--- lua/entities/wac_pl_jenny/shared.lua
+++ lua/entities/wac_pl_jenny/shared.lua
@@ -12,9 +12,9 @@ ENT.Instructions 		= ""
 ENT.Spawnable			= true
 ENT.AdminSpawnable	= true
 
-ENT.Model			= "models/Curtiss/jenny_jn4.mdl"
+ENT.Model			= "models/curtiss/jenny_jn4.mdl"
 ENT.RotorPhModel	= "models/props_junk/sawblade001a.mdl"
-ENT.RotorModel		= "models/Curtiss/jenny_prop.mdl"
+ENT.RotorModel		= "models/curtiss/jenny_prop.mdl"
 ENT.TopRotorDir        = 1
 ENT.TopRotorPos	= Vector(62, 0, 0)
 ENT.BackRotorPos	= Vector(8, 0, -48)
